36f15a401263ffc4b7790f7240e18674adcfb641
Victor Barrios <victor.barrios@bestsecret.com>
2022-08-18 15:35:26 +0200

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Barrios, Victor on 18/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concurrency
+//
+//  Created by Barrios, Victor on 18/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
